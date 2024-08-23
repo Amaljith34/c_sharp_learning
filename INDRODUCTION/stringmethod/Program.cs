f1658a31d29568aef2baa99048c262421342e228
Amaljith-aj <jith51541@gmail.com>
2024-08-23 16:59:30 +0530

    add method

--- INDRODUCTION/stringmethod/Program.cs
+++ INDRODUCTION/stringmethod/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            
+            //tolower
+            string orginal = "hello WORLD";
+            string lowercase=orginal.ToLower();
+            Console.WriteLine(lowercase);
+
+            //TOUPPER
+            string uppercase=orginal.ToUpper();
+            Console.WriteLine(uppercase);
+
+            //split
+            string originalfruts = "apple,banana,orange";
+            string[] fruits = originalfruts.Split(',');
+            foreach(var i in fruits) 
+            Console.WriteLine(i);
+
+            //length
+            int length=orginal.Length;
+            Console.WriteLine(length);
+
+            //substring
+            string newString = orginal.Replace("WORLD", "C#");
+            Console.WriteLine(newString);
         }
     }
 }
