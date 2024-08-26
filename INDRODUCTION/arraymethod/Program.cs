a961ffbfe59adef90dba4399f290cbbd882e0458
Amaljith-aj <jith51541@gmail.com>
2024-08-26 23:45:45 +0530

    add array method with small example

---
+++ INDRODUCTION/arraymethod/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace arraymethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //index of
+            int[] arr = { 2, 8, 6, 7 };
+            Array.Sort(arr);
+            
+            foreach(int i in arr)
+            {
+                Console.WriteLine(i);
+
+            }
+        }
+    }
+}
